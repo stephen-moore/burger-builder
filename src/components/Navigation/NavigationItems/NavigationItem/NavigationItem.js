2ad1f8fea4b609ec21076d7495f88e749af3c29a
Stephen Moore <s_moore2009@live.ie>
2021-01-21 20:35:36 +0000

    Created axios instance and set baseURL. Sending data to the firebase database. Added Spinner Handling Errors

--- src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
+++ src/components/Navigation/NavigationItems/NavigationItem/NavigationItem.js
@@ -1,9 +1,13 @@
 import React from "react";
 
-const navigationItems = () => (
-    <ul>
-        <li></li>
-    </ul>
+import classes from "./NavigationItem.module.css";
+
+const navigationItem = (props) => (
+    <li className={classes.NavigationItem}>
+        <a href={props.link}
+           className={props.active ? classes.active : null}>
+            {props.children}</a>
+    </li>
 )
 
-export default navigationItems;
+export default navigationItem;
